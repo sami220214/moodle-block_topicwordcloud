c9c0398e88ce7c686c728a3ed1b79f75e5d4a5ff
sami220214 <180633966+sami220214@users.noreply.github.com>
2026-06-24 13:13:22 +0300

    Update files

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_topicwordcloud';
 $plugin->version = 2026062400;
-$plugin->release = '1.0.';
+$plugin->release = '1.0.0';
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
